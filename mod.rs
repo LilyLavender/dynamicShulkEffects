a45c6bf049851c59e697e5a2b053701e16ffe845
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-10-31 12:34:29 -0500

    Optimizations

--- mod.rs
+++ mod.rs
@@ -25,78 +25,14 @@ unsafe fn after_image4_on_arg29_replace(lua_state: u64) {
 			if i == 0 {
 				let mut newSwordHash: u64 = 0x0;
 				let monado_type = WorkModule::get_int(boma, *FIGHTER_SHULK_INSTANCE_WORK_ID_INT_SPECIAL_N_TYPE);
-				if hitbox_params[0].get_int() == L2CValue::new_int(0x10884a8b70).get_int() { // tex_shulk_sword1
-					if monado_type == 0 {
-						newSwordHash = 0x15b74bbdad; // tex_shulk_sword1_jump
-					} else if monado_type == 1 {
-						newSwordHash = 0x1653856c49; // tex_shulk_sword1_speed
-					} else if monado_type == 2 {
-						newSwordHash = 0x174511df67; // tex_shulk_sword1_shield
-					} else if monado_type == 3 {
-						newSwordHash = 0x1712bd2dce; // tex_shulk_sword1_buster
-					} else if monado_type == 4 {
-						newSwordHash = 0x16e3d1b22b; // tex_shulk_sword1_smash
-					} else {
-						newSwordHash = 0x10884a8b70;
+				for current_tex in shulk_texs {
+					if hitbox_params[0].get_int() == L2CValue::new_int(hash40(current_tex)).get_int() { 
+						for (i, suffix) in (0i32..).zip(["jump", "speed", "shield", "buster", "smash"]) {
+							if monado_type == i {
+								newSwordHash = hash40(format!("{}_{}", current_tex, suffix).as_str());
+							}
+						}
 					}
-				} else if hitbox_params[0].get_int() == L2CValue::new_int(0x106644ea5c).get_int() { // tex_shulk_sword3
-					if monado_type == 0 {
-						newSwordHash = 0x15fa831ca6; // tex_shulk_sword3_jump
-					} else if monado_type == 1 {
-						newSwordHash = 0x16c41a7d60; // tex_shulk_sword3_speed
-					} else if monado_type == 2 {
-						newSwordHash = 0x170734d81a; // tex_shulk_sword3_shield
-					} else if monado_type == 3 {
-						newSwordHash = 0x1750982ab3; // tex_shulk_sword3_buster
-					} else if monado_type == 4 {
-						newSwordHash = 0x16744ea302; // tex_shulk_sword3_smash
-					} else {
-						newSwordHash = 0x106644ea5c;
-					}
-				} else if hitbox_params[0].get_int() == L2CValue::new_int(0x108f274f69).get_int() { // tex_shulk_sword5
-					if monado_type == 0 {
-						newSwordHash = 0x152cdaffbb; // tex_shulk_sword5_jump
-					} else if monado_type == 1 {
-						newSwordHash = 0x16a7ca485a; // tex_shulk_sword5_speed
-					} else if monado_type == 2 {
-						newSwordHash = 0x17c15bd19d; // tex_shulk_sword5_shield
-					} else if monado_type == 3 {
-						newSwordHash = 0x1796f72334; // tex_shulk_sword5_buster
-					} else if monado_type == 4 {
-						newSwordHash = 0x16179e9638; // tex_shulk_sword5_smash
-					} else {
-						newSwordHash = 0x108f274f69;
-					}
-				} else if hitbox_params[0].get_int() == L2CValue::new_int(0x14f8cdba70).get_int() { // tex_shulk_swordpink1
-					if monado_type == 0 {
-						newSwordHash = 0x193499569a; // tex_shulk_swordpink1_jump
-					} else if monado_type == 1 {
-						newSwordHash = 0x1aebbb1bad; // tex_shulk_swordpink1_speed
-					} else if monado_type == 2 {
-						newSwordHash = 0x1be2cec771; // tex_shulk_swordpink1_shield
-					} else if monado_type == 3 {
-						newSwordHash = 0x1bb56235d8; // tex_shulk_swordpink1_buster
-					} else if monado_type == 4 {
-						newSwordHash = 0x1a5befc5cf; // tex_shulk_swordpink1_smash
-					} else {
-						newSwordHash = 0x14f8cdba70;
-					}
-				} else if hitbox_params[0].get_int() == L2CValue::new_int(0x13723c0845).get_int() { // tex_shulk_swordred1
-					if monado_type == 0 {
-						newSwordHash = 0x189f498e5b; // tex_shulk_swordred1_jump
-					} else if monado_type == 1 {
-						newSwordHash = 0x1907733953; // tex_shulk_swordred1_speed
-					} else if monado_type == 2 {
-						newSwordHash = 0x1ab827d048; // tex_shulk_swordred1_shield
-					} else if monado_type == 3 {
-						newSwordHash = 0x1aef8b22e1; // tex_shulk_swordred1_buster
-					} else if monado_type == 4 {
-						newSwordHash = 0x19b727e731; // tex_shulk_swordred1_smash
-					} else {
-						newSwordHash = 0x13723c0845;
-					}
-				} else {
-					newSwordHash = hitbox_params[0].get_int();
 				}
 				l2c_agent.push_lua_stack(&mut L2CValue::new_int(newSwordHash)); 
 			} else {
